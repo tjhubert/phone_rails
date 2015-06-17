958ed856f26238f79b3d1599505bc50d434a57e3
tjhubert <tjhubert@live.com>
2015-06-17 11:22:14 -0700

    put secret stuff in secrets yml

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Integrate phone number into devise. Using twilio to send confirmation code to a phone number specified to confirm the phone number (currently it's only my number and intern Tyler's number that are registered. Only registered numbers work with twilio trial account)
 
-TODO: have a column for verified and unverified phone number, hide twilio sids and tokens, ensure methods don't expose user's information
+TODO: ensure methods don't expose user's information
 
--- app/helpers/verify_phone_number_helper.rb
+++ app/helpers/verify_phone_number_helper.rb
@@ -4,10 +4,10 @@ module VerifyPhoneNumberHelper
     number_to_send_to = user.phone_number
     verification_code = user.phone_verification_code
 
-    twilio_sid = "ACd35391c08cde7926e2295d1812ada918"
-    twilio_token = "44d79a36adb3d54cc15711d94d149119"
-    twilio_phone_number = "6502810746"
- 
+    twilio_sid = Rails.application.secrets.twilio_sid
+    twilio_token = Rails.application.secrets.twilio_token
+    twilio_phone_number = Rails.application.secrets.twilio_phone_number
+
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
     @twilio_client.account.sms.messages.create(
